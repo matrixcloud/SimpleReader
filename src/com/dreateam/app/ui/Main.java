9366edbe31012fc7a011f3ed184b2221a6d10e95
zcloud <zcloud00@gmail.com>
2014-01-27 12:50:19 +0800

    重构SimpleReader。

---
+++ src/com/dreateam/app/ui/Main.java
@@ -0,0 +1,23 @@
+package com.dreateam.app.ui;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Main extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
